32c4c4b8b31893759a0986975e3e966f86c03154
dishalodha <dishalodha@ebi.ac.uk>
2023-10-02 19:34:25 +0100

    creating a database by sqlalchemy

--- src/ensembl/rnaseq/registry/api.py
+++ src/ensembl/rnaseq/registry/api.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+import argschema
 
 """RNA-Seq registry API module."""
 
@@ -25,3 +27,40 @@ class RnaseqRegistry:
 
     def __init__(self, db: str) -> None:
         self.db = db
+        self.engine = create_engine(f'sqlite:///{db}', echo=True)
+        
+
+    def create_load(self):
+        metadata = MetaData()
+
+        dataset = Table(
+            'dataset', metadata,
+            Column('name', String, primary_key=True),
+            Column('organism', String))
+        
+        sample = Table(
+            'sample', metadata,
+            Column('SRA accession', String, primary_key=True),
+            Column('dataset', String))
+        
+        organism = Table(
+            'organism', metadata,
+            Column('organism_abbrv', String, primary_key=True),
+            Column('component', String))
+       
+        metadata.create_all(self.engine)
+
+class InputSchema(argschema.ArgSchema):
+    """Input arguments expected by the entry point of this module."""
+    dbname = argschema.fields.String(required=True, metadata={"descriptions": "Database name for sqllite"})
+
+def main():
+    """Main script entry-point."""
+    mod = argschema.ArgSchemaParser(schema_type=InputSchema)
+    registry = RnaseqRegistry(mod.args["dbname"])
+    registry.create_load()
+
+if __name__ == "__main__":
+    main()
+    
+
