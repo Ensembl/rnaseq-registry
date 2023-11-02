0df9556accbc115310316cd53dcf65c11d332d44
dishalodha <dishalodha@ebi.ac.uk>
2023-11-02 15:40:07 +0000

    correcting issues from ci/cd

--- src/ensembl/rnaseq/registry/database_schema.py
+++ src/ensembl/rnaseq/registry/database_schema.py
@@ -98,4 +98,4 @@ class Accession(Base):
     samples: Mapped[List[Sample]] = relationship(back_populates="accessions", cascade="all", lazy="joined")
 
     def __repr__(self) -> str:
-        return f"accession(sra_id={self.sra_id!r}, samples={self.samples.name!r})"
+        return f"accession(sra_id={self.sra_id!r}, samples={self.samples!r})"
